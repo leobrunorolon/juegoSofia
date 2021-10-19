2f7c17f3c2ef8aa5b5c0eac7b6b8f27c662d0880
leonardo Rolon <leobrunorolon@gmail.com>
2021-10-19 01:50:47 -0300

    Primer commit Juego Sofia

---
+++ juego.js
@@ -0,0 +1,56 @@
+document.getElementById(`unicornio`).addEventListener("click", sumarPuntos);
+document.getElementById(`nubes`).addEventListener("click", restarVida);
+document.getElementById(`nubesDos`).addEventListener("click", restarVida);
+
+let puntos = 0;
+let tiempo = 60;
+let score = 30;
+let vidas = 3;
+let vidasTotal = 3;
+function sumarPuntos() {
+  puntos++;
+  document.getElementById("puntos").innerHTML = `Puntos: ${puntos}/${score}`;
+  randnum = Math.round(Math.random() * 450);
+  randnumDos = Math.round(Math.random() * 500);
+  document.getElementById(`unicornio`).style.marginTop = randnum + "px";
+  document.getElementById(`unicornio`).style.marginLeft = randnumDos + "px";
+
+  if (puntos == 30) {
+    alert("Ganaste Sofia");
+    tiempo = 60;
+    puntos = 0;
+    vidas = 3;
+  }
+}
+function restarVida() {
+  vidas--;
+  document.getElementById(
+    "vidas"
+  ).innerHTML = `Corazones: ${vidas}/${vidasTotal}`;
+  if (vidas == 0) {
+    alert("Te quedaste sin vida");
+    tiempo = 60;
+    puntos = 0;
+    vidas = 3;
+  }
+}
+function restarTiempo() {
+  tiempo--;
+  document.getElementById("tiempo").innerHTML = `Tiempo: ${tiempo}`;
+  randnumTres = Math.round(Math.random() * 450);
+  randnumCuatro = Math.round(Math.random() * 500);
+  document.getElementById(`nubes`).style.marginTop = randnumTres + "px";
+  document.getElementById(`nubes`).style.marginLeft = randnumCuatro + "px";
+  randnumCinco = Math.round(Math.random() * 450);
+  randnumSeis = Math.round(Math.random() * 500);
+  document.getElementById(`nubesDos`).style.marginTop = randnumCinco + "px";
+  document.getElementById(`nubesDos`).style.marginLeft = randnumSeis + "px";
+  if (tiempo == 0) {
+    alert("Te quedaste sin tiempo vuelve a intentarlo Sofia");
+    tiempo = 60;
+    puntos = 0;
+    vidas = 3;
+  }
+}
+
+setInterval(restarTiempo, 1000);

c194b32b529a491c8eb6d9c70e3cc3220d35e089
leonardo Rolon <leobrunorolon@gmail.com>
2021-10-27 01:36:48 -0300

    se agrego main.js y local storage para guardar el nombre de usuario

---
+++ js/main.js
@@ -0,0 +1,29 @@
+// const inputs = document.querySelectorAll(`#formulario input`);
+
+// Letras, numeros, sin guion  guion_bajo y con caractares de 4 a 8
+const expresiones = {
+  usuarios: /^[a-zA-Z0-9]{4,8}$/,
+};
+
+// formulario.addEventListener(`submit`, validarUsuario());
+// const Usuario = [];
+
+document.getElementById(`ingreso`).addEventListener(`click`, (event) => {
+  event.preventDefault();
+  const usuario = document.getElementById(`usuario`).value;
+  localStorage.setItem("usuario", usuario);
+});
+// function validarUsuario() {
+//   if (expresiones.usuarios.test(usuario)) {
+//     document
+//       .getElementById(`section__usuario`)
+//       .classList.remove(`section__usuario-incorrecto`);
+//     document
+//       .getElementById(`section__usuario`)
+//       .classList.add(`section__usuario-correcto`);
+//   } else {
+//     document
+//       .getElementById(`section__usuario`)
+//       .classList.add(`section__usuario-incorrecto`);
+//   }
+// }
